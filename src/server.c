5f54876cc95d801bde1fa32e06cec4c903ea623e
Artur Harutyunyan <arturhar@outlook.com>
2024-04-06 01:14:41 +0400

    print huge text

--- src/server.c
+++ src/server.c
@@ -11,36 +11,67 @@
 /* ************************************************************************** */
 #include "../include/minitalk.h"
 
-void	receive_message(int signal)
-{
-	static int	bit;
-	static int	i;
+int message_count = 0;
 
-	if (signal == SIGUSR1)
-		i |= (0x01 << bit);
-	bit++;
-	if (bit == 8)
-	{
-		ft_printf("%c", i);
-		bit = 0;
-		i = 0;
-	}
+void receive_message(int signal)
+{
+    static int bit = 0;
+    static int i = 0;
+
+    if (signal == SIGUSR1)
+        i |= (0x01 << bit);
+    bit++;
+    if (bit == 8)
+    {
+        ft_printf("%c", i);
+        bit = 0;
+        i = 0;
+    }
 }
 
-int	main(int argc, char **argv)
+void print_minitalk(void)
 {
-	pid_t	pid;
+    ft_printf("\033[2J\033[1;1H");
+    ft_printf("\n\n\n");
+    ft_printf("                                                 ");
+    ft_printf("\033[1;34m");
+    ft_printf("███╗░░░███╗██╗███╗░░██╗██╗████████╗░█████╗░██╗░░░░░██╗░░██╗\n");
+    ft_printf("                                                 ");
+    ft_printf("████╗░████║██║████╗░██║██║╚══██╔══╝██╔══██╗██║░░░░░██║░██╔╝\n");
+    ft_printf("                                                 ");
+    ft_printf("██╔████╔██║██║██╔██╗██║██║░░░██║░░░███████║██║░░░░░█████═╝░\n");
+    ft_printf("                                                 ");
+    ft_printf("██║╚██╔╝██║██║██║╚████║██║░░░██║░░░██╔══██║██║░░░░░██╔═██╗░\n");
+    ft_printf("                                                 ");
+    ft_printf("██║░╚═╝░██║██║██║░╚███║██║░░░██║░░░██║░░██║███████╗██║░╚██\n");
+    ft_printf("                                                 ");
+    ft_printf("╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚═╝░░╚═╝\n");
+    ft_printf("                                                 ");
+    ft_printf("\033[0;36m");
+}
 
+void print_details(pid_t pid)
+{
+    ft_printf("         ");
+    ft_printf("Welcome to arturhar's Minitalk server!\n");
+    ft_printf("                                                 ");
+    ft_printf("                  ");
+    ft_printf("Server PID - %d", pid);
+    ft_printf("\n\n");
+}
+
+int main(int argc, char **argv)
+{
+    pid_t pid;
+    (void)argv;
     if (argc != 1)
     {
-        ft_printf("WRONG FORMAT!!!\n");
-        ft_printf("USAGE: ./server\n");
         return (1);
     }
-	pid = getpid();
-	ft_printf("Server PID - %d\n", pid);
-    (void)argv;
-    while (argc == 1)
+    pid = getpid();
+    print_minitalk();
+    print_details(pid);
+    while (1)
     {
         signal(SIGUSR1, receive_message);
         signal(SIGUSR2, receive_message);
